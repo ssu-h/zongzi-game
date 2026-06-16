77d1e003bf40ebd9c6adf833157710e3e97be34a
ssu-h <vicky320886@gmail.com>
2026-06-16 15:50:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,24 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// 💡 題目資料庫：可在這裡自由增減題目、修改名稱、正解與圖片網址
+// 💡 題目資料庫：你可以自由修改、增加或減少題目
 const questions = [
-  { id: 1, name: "阿嬤手工招牌南部粽", price: 75, img: "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?w=600" },
-  { id: 2, name: "極上黑金松露皇王粽", price: 380, img: "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=600" },
-  { id: 3, name: "頂級奢華鮑魚干貝皇朝粽", price: 1280, img: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=600" }
+  { 
+    id: 1, 
+    name: "【義美】經典台灣北部粽 (5入)", 
+    price: 395, 
+    img: "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?w=600" 
+  },
+  { 
+    id: 2, 
+    name: "【星巴克】夏日星冰粽禮盒", 
+    price: 680, 
+    img: "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=600" 
+  },
+  { 
+    id: 3, 
+    name: "【台北新板希爾頓酒店】頂級鮑魚干貝裹蒸粽", 
+    price: 1280, 
+    img: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=600" 
+  }
 ];
